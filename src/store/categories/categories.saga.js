f48dff8d53a2e689ddc0fd52d790de6c70626175
deniel980 <deniel98045@gmail.com>
2023-01-21 14:47:05 +0100

    the end is near

--- src/store/categories/categories.saga.js
+++ src/store/categories/categories.saga.js
@@ -1,27 +1,27 @@
-import { call, all, takeLatest, put } from "@redux-saga/core/effects";
+// import { call, all, takeLatest, put } from "@redux-saga/core/effects";
 
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./category.action";
-import { CATEGORIES_ACTION_TYPES } from "./category.types";
-
-
-export function * fetchCategoriesAsync(){
-    try {
-        const categoriesArray = yield call(getCategoriesAndDocuments,'categories');
-        yield put(fetchCategoriesSuccess(categoriesArray));
-    }catch (error) {
-        yield put(fetchCategoriesFailed(error));
-    }
-}
-
-export function* onFetchCategories(){
-    yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync)
-}
+// import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+// import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./category.action";
+// import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 
+// export function * fetchCategoriesAsync(){
+//     try {
+//         const categoriesArray = yield call(getCategoriesAndDocuments,'categories');
+//         yield put(fetchCategoriesSuccess(categoriesArray));
+//     }catch (error) {
+//         yield put(fetchCategoriesFailed(error));
+//     }
+// }
 
-export function* categoriesSaga(){
-    yield all([call(onFetchCategories)]);
+// export function* onFetchCategories(){
+//     yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync)
+// }
 
-}
+
+
+// export function* categoriesSaga(){
+//     yield all([call(onFetchCategories)]);
+
+// }
 
--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -7,7 +7,9 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  User,
   onAuthStateChanged,
+  NextOrObserver,
 } from 'firebase/auth';
 import {
   getFirestore,
@@ -18,8 +20,10 @@ import {
   writeBatch,
   query,
   getDocs,
+  QueryDocumentSnapshot,
 } from 'firebase/firestore';
 
+import { Category } from '../../store/categories/category.types';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBjGeIZUi3p-LWdnWOk--hHNcn9XNV9a6o",
@@ -46,13 +50,18 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
-export const addCollectionAndDocuments = async (
-  collectionKey,
-  objectsToAdd,
-  field
-) => {
+
+export type ObjectToAdd = {
+  title: string;
+}
+
+export const addCollectionAndDocuments = async <T extends ObjectToAdd>(
+  collectionKey: string,
+  objectsToAdd: T[],
+): Promise<void> => {
   const collectionRef = collection(db, collectionKey);
   const batch = writeBatch(db);
+
 
   objectsToAdd.forEach((object) => {
     const docRef = doc(collectionRef, object.title.toLowerCase());
@@ -63,18 +72,32 @@ export const addCollectionAndDocuments = async (
   console.log('done');
 };
 
-export const getCategoriesAndDocuments = async () => {
+
+export const getCategoriesAndDocuments = async (): Promise<Category[]> => {
   const collectionRef = collection(db, 'categories');
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
+  return querySnapshot.docs.map(
+    (docSnapshot) => docSnapshot.data() as Category
+    );
 };
 
+export type AdditionalInformation = {
+  displayName?: string;
+}
+
+export type UserData = {
+  displayName: string;
+  createdAt: Date;
+  email: string;
+}
+
+
 export const createUserDocumentFromAuth = async (
-  userAuth,
-  additionalInformation = {}
-) => {
+  userAuth: User,
+  additionalInformation = {} as AdditionalInformation
+): Promise<void | QueryDocumentSnapshot<UserData>> => {
   if (!userAuth) return;
 
   const userDocRef = doc(db, 'users', userAuth.uid);
@@ -93,20 +116,20 @@ export const createUserDocumentFromAuth = async (
         ...additionalInformation,
       });
     } catch (error) {
-      console.log('error creating the user', error.message);
+      console.log('error creating the user', error);
     }
   }
 
-  return userSnapshot;
+  return userSnapshot as QueryDocumentSnapshot<UserData>;
 };
 
-export const createAuthUserWithEmailAndPassword = async (email, password) => {
+export const createAuthUserWithEmailAndPassword = async (email:string, password:string) => {
   if (!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
 };
 
-export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+export const signInAuthUserWithEmailAndPassword = async (email:string, password:string) => {
   if (!email || !password) return;
 
   return await signInWithEmailAndPassword(auth, email, password);
@@ -114,10 +137,10 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
 export const signOutUser = async () => await signOut(auth);
 
-export const onAuthStateChangedListener = (callback) =>
+export const onAuthStateChangedListener = (callback: NextOrObserver<User>) =>
   onAuthStateChanged(auth, callback);
 
-export const getCurrentUser = () => {
+export const getCurrentUser = (): Promise<User|null> => {
   return new Promise((resolve, reject) => {
     const unsubscribe = onAuthStateChanged(
       auth,
